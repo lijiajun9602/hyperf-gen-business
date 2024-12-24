1443c5e26b6166f6dc975e04995ee27d69d66540
admin <396948873@qq.com>
2024-12-24 15:50:51 +0800

    初始化

--- src/Common/Dto/UserJwtAuthIn.php
+++ src/Common/Dto/UserJwtAuthIn.php
@@ -4,12 +4,16 @@ namespace Hyperf\GenBusiness\Common\Dto;
 
 
 
+use Hyperf\ApiDocs\Annotation\ApiModel;
 use Hyperf\ApiDocs\Annotation\ApiModelProperty;
+use Hyperf\DTO\Annotation\Dto;
 use Hyperf\PhpAccessor\Annotation\HyperfData;
 use PhpAccessor\Attribute\Data;
 
 #[HyperfData]
 #[Data]
+#[Dto]
+#[ApiModel(value: 'UserJwtAuthIn入参')]
 class UserJwtAuthIn
 {
     #[ApiModelProperty(value: '用户ID Token获得', hidden: true)]
