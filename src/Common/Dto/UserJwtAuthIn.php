f88e3e8964fda4220c5daebf5fa00918b528a1b2
admin <396948873@qq.com>
2024-12-25 09:07:55 +0800

    初始化

--- src/Common/Dto/UserJwtAuthIn.php
+++ src/Common/Dto/UserJwtAuthIn.php
@@ -10,14 +10,44 @@ use Hyperf\DTO\Annotation\Dto;
 use Hyperf\PhpAccessor\Annotation\HyperfData;
 use PhpAccessor\Attribute\Data;
 
-#[HyperfData]
-#[Data]
 #[Dto]
 #[ApiModel(value: 'UserJwtAuthIn入参')]
 class UserJwtAuthIn
 {
     #[ApiModelProperty(value: '用户ID Token获得', hidden: true)]
     public int $userId;
+
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     */
+    public function setUserId(int $userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNickName(): string
+    {
+        return $this->nickName;
+    }
+
+    /**
+     * @param string $nickName
+     */
+    public function setNickName(string $nickName): void
+    {
+        $this->nickName = $nickName;
+    }
     #[ApiModelProperty(value: '用户昵称', hidden: true)]
     public string $nickName;
 }
